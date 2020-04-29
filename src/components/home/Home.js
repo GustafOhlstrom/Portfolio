5a7b1858c33c5dceded23c843e2caa4a81ec60d2
Gustaf Ohlström <gustaf_ohlstrom@hotmail.com>
2020-04-29 16:02:28 +0200

    Update to home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,29 @@
+import './home.scss'
+import quizmasterMobile from '../../assets/images/quizmasterMobile.PNG'
+import quizmasterDesktop from '../../assets/images/quizmasterDesktop4.PNG'
 import React from 'react'
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <div id="home">
+            <h1>Featured Projects</h1>
+
+            <div className="grid">
+                <div className="project one">
+                    <img className="project-img" src={quizmasterDesktop} alt=""/>
+                    <ul className="project-tags">
+                        <li>React</li>
+                        <li>JSX</li>
+                        <li>SASS</li>
+                        <li>Firebase</li>
+                    </ul>
+                    
+                </div>
+                <div className="project two">
+                    <img className="project-img" src={quizmasterDesktop} alt=""/>
+                </div>
+            </div>
+        </div>
     )
 }
 
