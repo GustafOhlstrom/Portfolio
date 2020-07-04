1c52f3ce0da451fd33008b1278ee4e55303d1d08
Gustaf Ohlstrom <gustaf_ohlstrom@hotmail.com>
2020-07-04 14:22:09 +0200

    Added Kill The Virus to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,5 @@
 import './projects.scss'
+import killTheVirus from '../../assets/images/kill-the-virus.png'
 import quizes from '../../assets/images/quizes.png'
 import typingSpeed from '../../assets/images/typing-speed.png'
 import React from 'react'
@@ -9,9 +10,17 @@ function Projects() {
             <h3>Projects</h3>
 
             <div className="grid">
+
+                <a className="project" href="http://kill-the-virus.gustafohlstrom.se/">
+                    <img className="project-img" src={killTheVirus} alt="Kill The Virus Website"/>
+                    <div className="project-info">
+                        <h2>Kill The Virus</h2>
+                        <p>Socket.io, Node, Express, React JS, SASS</p>
+                    </div>
+                </a>
                 
                 <a className="project" href="http://quizes.gustafohlstrom.se/">
-                    <img className="project-img" src={quizes} alt=""/>
+                    <img className="project-img" src={quizes} alt="Quiz Website"/>
                     <div className="project-info">
                         <h2>Quizes</h2>
                         <p>React JS, SASS, Firebase</p>
@@ -19,17 +28,13 @@ function Projects() {
                 </a>
 
                 <a className="project" href="http://typingspeed.gustafohlstrom.se/">
-                    <img className="project-img" src={typingSpeed} alt=""/>
+                    <img className="project-img" src={typingSpeed} alt="Typing Speed Test Website"/>
                     <div className="project-info">
                         <h2>Typing Speed</h2>
                         <p>React JS, SASS</p>
                     </div>
                 </a>
 
-                <div className="project more-projects" href="http://typingspeed.gustafohlstrom.se/">
-                    <p>More projects coming soon</p>
-                </div>
-            
             </div>
         </section>
     )
