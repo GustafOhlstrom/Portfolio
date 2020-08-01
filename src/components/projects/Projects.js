167aa92b7cbb21ede1f6ffd42743778d05e9ba45
Gustaf Ohlstrom <gustaf_ohlstrom@hotmail.com>
2020-08-01 02:22:12 +0200

    Add description on hover for projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import './projects.scss'
 import killTheVirus from '../../assets/images/kill-the-virus.png'
-import quizes from '../../assets/images/quizes.png'
+import quizzes from '../../assets/images/quizes.png'
 import typingSpeed from '../../assets/images/typing-speed.png'
 import React from 'react'
 
@@ -15,15 +15,17 @@ function Projects() {
                     <img className="project-img" src={killTheVirus} alt="Kill The Virus Website"/>
                     <div className="project-info">
                         <h2>Kill The Virus</h2>
-                        <p>Socket.io, Node, Express, React JS, SASS</p>
+                        <p className="project-tags">Socket.io, Node, Express, React JS, SASS</p>
+                        <p className="project-description">A realtime reaction based game that puts two opponents in a head to head. The one who can kill the virus the fastest wins.</p>
                     </div>
                 </a>
                 
-                <a className="project" href="http://quizes.gustafohlstrom.se/">
-                    <img className="project-img" src={quizes} alt="Quiz Website"/>
+                <a className="project" href="http://quizzes.gustafohlstrom.se/">
+                    <img className="project-img" src={quizzes} alt="Quiz Website"/>
                     <div className="project-info">
-                        <h2>Quizes</h2>
-                        <p>React JS, SASS, Firebase</p>
+                        <h2>Quizzes</h2>
+                        <p className="project-tags">React JS, SASS, Firebase</p>
+                        <p className="project-description">Create and take quizzes with any amount of correct answers and points.</p>
                     </div>
                 </a>
 
@@ -31,7 +33,8 @@ function Projects() {
                     <img className="project-img" src={typingSpeed} alt="Typing Speed Test Website"/>
                     <div className="project-info">
                         <h2>Typing Speed</h2>
-                        <p>React JS, SASS</p>
+                        <p className="project-tags">React JS, SASS</p>
+                        <p className="project-description">Test how fast you can type.</p>
                     </div>
                 </a>
 
